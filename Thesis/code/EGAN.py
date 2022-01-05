4d572ec9ce99cc4683109867ea2c82f7a038ea19
ashishdhyani <me.dhyani@gmail.com>
2022-01-05 21:55:00 +0800

    remaining required files

--- Thesis/code/EGAN.py
+++ Thesis/code/EGAN.py
@@ -4,13 +4,29 @@ import os
 
 
 class EGAN:
-    def __init__(self, srcImg):
+    def __init__(self, frame):
+		self.frame = frame
         initialize()
     
     def initialize():
         print("initialized")
     
     def illuminate():
-        new_frame = cv2.LUT(frame, LU_table)
-        return new_frame
+	
+		input = 'models/EGAN/test_dataset/testA/predict.png'
+		output = 'models/EGAN/test_dataset/testB/predict.png'
+		if os.path.isfile(input)
+			os.remove(input)
+			
+		if os.path.isfile(output)
+			os.remove(output)
+
+		cv2.imwrite(input, self.frame)
+		cv2.waitKey(0)
+		
+        os.system("python models/EGAN/scripts/script.py --predict");
+		
+		frame = cv2.imread(output)
+		
+        return frame
 
--- Thesis/code/KIND.py
+++ Thesis/code/KIND.py
@@ -11,5 +11,20 @@ class KIND:
         print("initialized")
     
     def illuminate():
-        return self.frame
+		input = 'models/KiND/test/predict.png'
+		output = 'models/KiND/results/test/predict.png'
+		if os.path.isfile(input)
+			os.remove(input)
+			
+		if os.path.isfile(output)
+			os.remove(output)
+
+		cv2.imwrite(input, self.frame)
+		cv2.waitKey(0)
+		
+        os.system("python models/KiND/scripts/evaluate.py");
+		
+		frame = cv2.imread(output)
+		
+        return frame
 
